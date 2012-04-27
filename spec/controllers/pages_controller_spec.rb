6a1ef509afff10916328dd9f30e06273fa57fd34
Ketan Deshmukh <ketan.davvindore@gmail.com>
2012-04-27 18:33:27 +0530

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
+    render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+   
+    
   end
 
   describe "GET 'contact'" do
@@ -15,5 +18,13 @@ describe PagesController do
       response.should be_success
     end
   end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
 
 end
